a24a82a194082193bfdbeb3045659851072665a2
Pa Lee <sheeyeechang@gmail.com>
2019-06-12 21:41:38 -0500

    Added void InsertSweepstakes (Sweepstakes sweepstakes) and Sweepstakes GetSweepstakes() methods for ISweepstakesManager interface.

--- SweepstakesProject/ISweepstakesManager.cs
+++ SweepstakesProject/ISweepstakesManager.cs
@@ -6,26 +6,16 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-    class ISweepstakesManager
+    interface ISweepstakesManager
     {
-        //member variable (HAS A)
+        void InsertSweepstakes(Sweepstakes sweepstakes);
+        Sweepstakes GetSweepstakes();
 
-        //constructor
-        public ISweepstakesManager()
-        {
 
-        }
         //method (CAN DO)  ------------------------------------------------------------------------------------------------
         //(10 points) As a developer, I want to write an ISweepstakesManager interface with the following methods 
         //for a sweepstakes management system:
-        public void InsertSweepstakes(Sweepstakes sweepstakes)
-        {
 
-        }
-        //public void Sweepstakes GetSweepstakes()
-        //{
-            
-        //}
 
 
 
